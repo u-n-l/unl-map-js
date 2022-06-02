284bc660231dc3eb68d39f1ca449a34831bee239
Anda Papita <Anda.Papita@rebeldot.com>
2022-06-02 19:58:45 +0300

    feat(sdk): add styles for map tiles switch

--- src/controls/MapTilesControl/MapTilesControl.ts
+++ src/controls/MapTilesControl/MapTilesControl.ts
@@ -2,6 +2,7 @@ import Base from "../Base/Base";
 import ControlButton from "../GridControl/components/ControlButton";
 import { getStyles } from "../../map/styles/here";
 import { StyleSpecification } from "maplibre-gl";
+import mapTilesTooltip, { getButtonIcon } from "./mapTilesTooltip";
 
 export interface MapTilesControlOptions {
   styles?: StyleSpecification[];
@@ -11,54 +12,51 @@ export interface MapTilesControlOptions {
 export default class MapTilesControl extends Base {
   styles: StyleSpecification[];
   onChange?: (style: StyleSpecification) => void;
-  buttons: ControlButton[];
+  button: ControlButton;
+  tooltip: HTMLDivElement;
+  mapTilesButtons: HTMLButtonElement[];
 
   constructor(options?: MapTilesControlOptions) {
     super();
 
     this.styles = options?.styles ?? this.defaultOptions;
     this.onChange = options?.onChange;
-    this.buttons = [];
+    this.mapTilesButtons = [];
+
+    this.button = new ControlButton().setIcon(
+      //@ts-ignore
+      getButtonIcon(this.defaultOptions[0].name)
+    );
+
+    this.tooltip = mapTilesTooltip(
+      this.styles,
+      (style) => {
+        this.map.setStyle(style);
+        //@ts-ignore
+        this.button.setIcon(getButtonIcon(style.name));
+      },
+      this.mapTilesButtons
+    );
   }
 
   insert() {
-    this.styles.forEach((style) => {
-      const button = new ControlButton();
-      //@ts-ignore
-      button.setText(style.label);
-      button.onClick(() => {
-        if (button.isActive()) return;
-        this.map.setStyle(style);
-        if (this.onChange) this.onChange(style);
-      });
-      this.buttons.push(button);
-      this.addButton(button);
-    });
+    this.button.onClick(this.showMapTiles);
+    this.addButton(this.button);
 
-    this.map.on("styledata", () => {
-      this.buttons.forEach((button) => {
-        button.setActive(false);
-      });
-      const styleNames = this.styles.map((style) => style.name);
-      const styleName = this.map.getStyle().name;
-      if (!styleName) throw Error("style must have name");
-      const currentStyleIndex = styleNames.indexOf(styleName);
-      if (currentStyleIndex !== -1) {
-        const currentButton = this.buttons[currentStyleIndex];
-        currentButton.setActive(true);
-      }
-    });
+    this.map.getContainer().appendChild(this.tooltip);
   }
 
   get defaultOptions(): any[] {
     return getStyles("pFlhWNivCejOEWnTKIQf6ZKRWP5avfiANvleJKR0XAY");
   }
 
+  showMapTiles = () => {
+    this.tooltip.style.display = "block";
+  };
+
   onAddControl = () => {
     this.insert();
   };
 
-  onRemoveControl = () => {
-    console.log("a");
-  };
+  onRemoveControl = () => {};
 }
