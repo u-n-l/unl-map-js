e2029a601ec6d211e8ed484f50ce1888999e173d
Lorena <lorena.caba@rebeldot.com>
2022-10-17 11:20:50 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import * as Maplibre from "maplibre-gl";
-import * as Controls from "./controls";
-import UnlMap from "./map/UnlMap";
 
 const exported = {
   ...Maplibre,
-  Map: UnlMap,
-  ...Controls,
 };
 
 export default exported;
